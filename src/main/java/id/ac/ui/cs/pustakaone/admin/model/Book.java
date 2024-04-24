f4b4fbad9d2d3f0998f369721891be2afaf0f20e
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-04-24 19:58:13 +0700

    [RED] add Book and BookCart skeleton to read data

---
+++ src/main/java/id/ac/ui/cs/pustakaone/admin/model/Book.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.pustakaone.admin.model;
+
+public class Book {
+    private String id;
+
+    public Book(String id) {
+        this.id = id;
+    }
+    public String getId(){
+        return id;
+    }
+}
