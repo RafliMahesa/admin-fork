dc1372e5c69ded5c6b09785f850aa3b824cb0b44
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-05-01 13:08:06 +0700

    [REFACTOR] Implement log using jpa

--- src/main/java/id/ac/ui/cs/pustakaone/admin/model/Log.java
+++ src/main/java/id/ac/ui/cs/pustakaone/admin/model/Log.java
@@ -1,16 +1,29 @@
 package id.ac.ui.cs.pustakaone.admin.model;
+import jakarta.persistence.*;
+import java.util.UUID;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "Log")
 public class Log {
-    
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "action")
     String action;
+
+    @Column(name = "date")
     String date;
 
     public Log(String action, String date){
+        this.id = UUID.randomUUID().toString();
         this.setAction(action);
         this.setDate(date);
     }
