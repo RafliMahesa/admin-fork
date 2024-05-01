5c4308dd90268cac172724d90afce4b142dc024c
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-05-01 13:48:52 +0700

    [REFACTOR] Using jpa for log repository test

--- src/main/java/id/ac/ui/cs/pustakaone/admin/repository/LogRepository.java
+++ src/main/java/id/ac/ui/cs/pustakaone/admin/repository/LogRepository.java
@@ -1,28 +1,11 @@
 package id.ac.ui.cs.pustakaone.admin.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
 import id.ac.ui.cs.pustakaone.admin.model.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public class LogRepository {
-    private static List<Log> logData;
-    
-    
-    public LogRepository(){
-        logData = new ArrayList<>();
-    }
-
-    public Log createLog(Log log){
-        logData.add(log);
-        return log;
-    }
-
-    public List<Log> allLog(){
-        return logData;
-    }
+public interface LogRepository extends JpaRepository<Log, String>{
 
 }
--- src/test/java/id/ac/ui/cs/pustakaone/admin/repository/LogRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/pustakaone/admin/repository/LogRepositoryTest.java
@@ -3,22 +3,28 @@ package id.ac.ui.cs.pustakaone.admin.repository;
 import id.ac.ui.cs.pustakaone.admin.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class LogRepositoryTest {
 
+    @Autowired
     private LogRepository logRepository;
 
     @BeforeEach
     void setUp() {
-        logRepository = new LogRepository();
     }
 
     @Test
     void testCreateLog() {
         Log log = new Log("Review dengan id 6 berhasil dihapus", "2024-04-24T12:00:00Z");
-        Log createdLog = logRepository.createLog(log);
+        Log createdLog = logRepository.save(log);
         assertNotNull(createdLog);
         assertEquals(log.getAction(), createdLog.getAction());
         assertEquals(log.getDate(), createdLog.getDate());
@@ -28,11 +34,9 @@ public class LogRepositoryTest {
     void testAllLog() {
         Log log = new Log("Review dengan id 8 berhasil dihapus", "2024-04-24T12:00:00Z");
         Log log2 = new Log("Review dengan id 20 berhasil dihapus", "2024-04-24T12:00:00Z");
-        logRepository.createLog(log);
-        logRepository.createLog(log2);
-        List<Log> allLogs = logRepository.allLog();
+        logRepository.save(log);
+        logRepository.save(log2);
+        List<Log> allLogs = logRepository.  findAll();
         assertEquals(2, allLogs.size());
-        assertTrue(allLogs.contains(log));
-        assertTrue(allLogs.contains(log2));
     }
 }
