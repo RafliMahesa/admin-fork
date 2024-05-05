33278c0558082bbba8ff6e1d6da645dfccc3857a
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-05-05 19:49:57 +0700

    [REFACTOR] remove unusable import

--- src/main/java/id/ac/ui/cs/pustakaone/admin/repository/AdminRepository.java
+++ src/main/java/id/ac/ui/cs/pustakaone/admin/repository/AdminRepository.java
@@ -1,8 +1,5 @@
 package id.ac.ui.cs.pustakaone.admin.repository;
 
-import id.ac.ui.cs.pustakaone.admin.service.LogDeleteService;
-import id.ac.ui.cs.pustakaone.admin.service.LogUpdateService;
-
 import java.util.HashMap;
 
 import org.springframework.http.*;
